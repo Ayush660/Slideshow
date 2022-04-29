a219a3ea9c53f59aeebbfc4e2267d7a5e88ad618
AYUSH <ayushhn2001@gmail.com>
2022-04-29 13:48:31 +0530

    Added objects

--- carousel.js
+++ carousel.js
@@ -1,55 +1,55 @@
-let slideIndex = 1;
-let timer;
-showSlide(slideIndex);
-timer = setInterval(nextSlide, 4000);
+/* Code by wrapping all the functions into an object */
 
-document.onkeydown = function(e) {
-    if(e.key === "ArrowLeft") {
-        prevSlide();
-    } else if(e.key === "ArrowRight") {
-        nextSlide();
+const slideShow = {
+    slideIndex: 1,
+    timer: null,
+    setTimer: function() {
+        return setInterval(() => {
+            this.changeSlide(1);
+        }, 4000);
+    },
+    changeSlide: function(n) {
+        clearInterval(this.timer);
+        this.slideIndex += n;
+        this.showSlide(this.slideIndex);
+
+        this.timer = this.setTimer();
+    },
+    current: function(n) {
+        clearInterval(this.timer)
+        this.slideIndex = n;
+        this.showSlide(this.slideIndex);
+
+        this.timer = this.setTimer();
+    },
+    key: function(event) {
+        if (event.key === "ArrowLeft") {
+            this.changeSlide(-1);
+        } else if (event.key === "ArrowRight") {
+            this.changeSlide(1);
+        }
+    },
+    showSlide: function(n) {
+        {
+            const slides = document.getElementsByClassName("slide");
+            const dots = document.getElementsByClassName("dot");
+            if (n > slides.length) this.slideIndex = 1;
+            else if (n < 1) this.slideIndex = slides.length;
+
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+
+            slides[this.slideIndex - 1].style.display = "block";
+            dots[this.slideIndex - 1].className += " active";
+
+        }
     }
 }
 
-function nextSlide() {
-    clearInterval(timer);
-    slideIndex += 1;
-    showSlide(slideIndex);
-
-    timer = setInterval(nextSlide, 4000);
-}
-
-function prevSlide() {
-    clearInterval(timer);
-    slideIndex -= 1;
-    showSlide(slideIndex);
-
-    timer = setInterval(nextSlide, 4000);
-}
-
-function current(n) {
-    clearInterval(timer)
-    slideIndex = n;
-    showSlide(slideIndex);
-
-    timer = setInterval(nextSlide, 4000);
-}
-
-function showSlide(n) {
-    const slides = document.getElementsByClassName("slide");
-    const dots = document.getElementsByClassName("dot");
-    if(n > slides.length) slideIndex = 1;
-    else if(n < 1) slideIndex = slides.length;
-
-    for(let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for(let i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-
-}
+slideShow.showSlide(1);
+slideShow.timer = slideShow.setTimer();
+document.addEventListener("keydown", (event) => {
+    slideShow.key(event);
+})
